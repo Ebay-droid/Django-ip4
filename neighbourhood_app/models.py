9194f90b66bb160d913fdc03108a9c44bd5cf731
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-10-31 22:34:47 +0300

    models added

--- neighbourhood_app/models.py
+++ neighbourhood_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Neighbourhood(models.Model):
+  name = models.CharField(max_length=200)
+  contacts_health = models.CharField(max_length=200)
+  contacts_police =models.CharField(max_length=200)
+  
+  
+class Business(models.Model) :
+  name = models.CharField(max_length=200)
+  address = models.CharField(max_length=200)
+  neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE) 
+  
+  
+class  Profile(models.Model):
+  profile_pic = CloudinaryField('image')
+  Bio = models.TextField()
+  email = models.EmailField()
+  phone_number = models.IntegerField(null=True)
+  user = models.ForeignKey(User,on_delete=models.CASCADE,default='')
+  neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)  
