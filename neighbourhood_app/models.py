b24301b1b218d581e889b237754565990877605d
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-11-01 09:34:19 +0300

    profile page added

--- neighbourhood_app/models.py
+++ neighbourhood_app/models.py
@@ -4,12 +4,21 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 # Create your models here.
 
+class  User(models.Model):
+  id =models.AutoField
+  name =models.CharField(max_length=20)
+  email =models.EmailField()
+  neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)  
+
+class Admin(models.Model):
+  
 
 class Neighbourhood(models.Model):
   name = models.CharField(max_length=200)
   contacts_health = models.CharField(max_length=200)
   contacts_police =models.CharField(max_length=200)
-  
+  occupants_count =models.IntegerField()
+  admin = models.ForeignKey(Admin,on_delete=models.CASCADE)
   
 class Business(models.Model) :
   name = models.CharField(max_length=200)
@@ -23,4 +32,6 @@ class  Profile(models.Model):
   email = models.EmailField()
   phone_number = models.IntegerField(null=True)
   user = models.ForeignKey(User,on_delete=models.CASCADE,default='')
-  neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)  
+  
+  
+  
