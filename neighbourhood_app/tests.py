47dcf58512b11265b8c8849f28c84f301c81a3bd
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-11-05 11:01:49 +0300

    few changes in test

--- neighbourhood_app/tests.py
+++ neighbourhood_app/tests.py
@@ -33,7 +33,19 @@ class NeighbourhoodTest(TestCase):
     
     
   def test_find_by_id(self):
-    self.test.sa  
+    self.test.save_neighbourhood()
+    self.test_1=Neighbourhood(name='Kikuyu',location='kenya',occupants_count=10,admin=user,contact_police=999,contact_fire=0213652471,contact_health=5202541102)  
 
-  # def tearDown(self):
-  #   Project.objects.all().delete()
+  def tearDown(self):
+    Project.objects.all().delete()
+    
+    
+class  ProfileTest(TestCase):
+  def setUp(self):
+    user = User.objects.create(username='test',password = 'hood')
+    neighbourhood = Neighbourhood.objects.create(name='Kabete',location='KENYA',occupants_count=20,admin=user,contact_police=999,contact_health=526325,contact_fire=02202522)
+    self.test = Profile(Bio='woohoo',user=user,phone_number=072525252,neighbourhood=neighbourhood)
+    
+  def test_instance(self):
+    self.assertTrue(isinstance(self.test,Profile))
+        
